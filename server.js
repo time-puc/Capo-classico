e1e721f50a14a1fd66d1fca69f3c77a33a8f4686
Bruno Amorim <bruno.amorim032@gmail.com>
2021-10-29 10:44:05 -0300

    cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-    return res.json({ message: 'ok'})
+    return res.json([
+        { name: "jeff"},
+        { name: "amorim"}
+    ])
 } )
 
 app.listen('4000')
