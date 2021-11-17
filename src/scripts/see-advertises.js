73b27907312e485065a11ee3adc2f1c8d57bb268
Amorim da Silva <amorim@energiaautomacao.com.br>
2021-11-17 08:44:50 -0300

    Botão enviar mensagem funcional com phone fixo

---
+++ src/scripts/see-advertises.js
@@ -0,0 +1,6 @@
+function sendMessage() {
+    var phone = '5511996533334'
+    var message = document.querySelector('#mensagem-form').value
+
+    window.open(`https://api.whatsapp.com/send?phone=${phone}&text=${message}`);
+}
