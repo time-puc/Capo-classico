76198e6a12a74cbc24c0304c7041a25ea3183a8e
Renata Vanetta <renatavanetta@users.noreply.github.com>
2021-11-17 21:31:16 -0300

    dbconnection feita

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const db = require('./db')
+const app = require('./dbconnection/server');
+
+//routes
+var announceRoute = require('./src/routes/advertise')(app);
+
+app.listen(3000, function(){
+    console.log('Server ON');
+});
