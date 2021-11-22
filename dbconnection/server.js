50e96c9a4a7933c821ad57ddb674f80da985eeab
Renata Vanetta <renatavanetta@users.noreply.github.com>
2021-11-22 17:09:58 -0300

    modificacoes corretas

--- dbconnection/server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', './src/views');
-
-module.exports = app;
--- src/scripts/advertise.js
+++ src/scripts/advertise.js
@@ -1,5 +1,24 @@
+var dbConnection = require('../../dbconnection/dbConnection');
+var connection = dbConnection();
+
+function allBrands() {
+    connection.query('select * from marca', function(errors, result){
+        var brands = JSON.parse(JSON.stringify(result));
+        for(let i=0; i<=brands.length; i++){
+            var option = document.createElement("option");
+            option.value = brands[i];
+            option.text = brands.charAt(0).toUpperCase();
+            var select = document.getElementsById('brand');
+            select.appendChild(option);
+        };
+    });
+    //loadBrands(brands);
+};
+
+
 // Icones da página
 window.onload = function() {
-    feather.replace()
+    console.log('carregado');
+    feather.replace();
+    allBrands();
 }
-
