1a187304a4153f209df3fd81d2d6ee3f9c502cce
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2021-11-24 18:26:00 -0300

    See advertise backend

--- src/scripts/see-advertises.js
+++ src/scripts/see-advertises.js
@@ -1,13 +1,44 @@
+var resp
+var modelo
+var versao
 
 function sendMessage() {
-    var phone = '5511996533334'
-    var message = document.querySelector('#mensagem-form').value
+    window.open(`mailto:${resp.email}?subject=Tenho interesse no veículo ${resp.modelo} ${resp.versao}&body=${document.querySelector('#mensagem-form').value}`);
+    //var phone = '5511996533334'
+    //var message = document.querySelector('#mensagem-form').value
 
-    alert('Você será encaminhado ao whatsapp do anunciante')
-    window.open(`https://api.whatsapp.com/send?phone=${phone}&text=${message}`);
+    //alert('Você será encaminhado ao whatsapp do anunciante')
+    //window.open(`https://api.whatsapp.com/send?phone=${phone}&text=${message}`);
+}
+
+function loadAdvertise(id) {
+    let url = `http://localhost:3000/anuncio/` + id
+
+    axios.get(url)
+    .then(response => {
+        dados = response.data[0]
+        resp = dados
+        document.getElementById('title-advertise').innerHTML = `${dados.modelo}`
+        document.getElementById('engine-advertise').innerHTML = `${dados.motor}`
+        document.getElementById('version-advertise').innerHTML = `${dados.versao}`
+        document.getElementById('year-advertise').innerHTML = `${dados.ano}`
+        document.getElementById('mileage-advertise').innerHTML = `${dados.quilometragem} Km`
+        document.getElementById('colour-advertise').innerHTML = `${dados.cor}`
+        document.getElementById('fuel-advertise').innerHTML = `${dados.combustivel}`
+        document.getElementById('condition-advertise').innerHTML = `${dados.condicao}`
+        document.getElementById('door-advertise').innerHTML = `${dados.porta}`
+        document.getElementById('gearbox-advertise').innerHTML = `${dados.transmissao}`
+        document.getElementById('description-advertise').innerHTML = `${dados.descricao}`
+        document.getElementById('price-advertise').innerHTML = `R$ ${dados.preco}`
+    })
+    .catch(error => {
+        alert(error.message)
+    })
+
+    event.preventDefault();
 }
 
 // Icones da página
 window.onload = function() {
-    feather.replace()
+    loadAdvertise(1)
 }
